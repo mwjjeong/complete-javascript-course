9ab578a93e76cfd2be99af9d1fba0a47de81b33a
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-10-08 15:45:17 +0900

    Complete the section `01 Fundamentals Part 1`

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,34 +1,105 @@
 // LECTURE: Values and Variables
 const country = "South Korea";
 const continent = "Asia";
-let population = 50;
+let population = 48;
 
 console.log(country, continent, population);
 
 // LECTURE: Data Types
 // LECTURE: let, const, and var
 const isIsland = false;
-const language = 'Korean';
-console.log(typeof isIsland, typeof population, typeof country, typeof language);
+const language = "English";
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language
+);
 // language = 'English'; -> TypeError!!
 
 // LECTURE: Basic Operators
-console.log('1: ', population / 2);
+console.log("1: ", population / 2);
 
 population++;
-console.log('2: ', population);
+console.log("2: ", population);
 
-let finlandPopulation = 6;
-console.log('3: ', finlandPopulation < population);
+const finlandPopulation = 6;
+console.log("3: ", finlandPopulation < population);
 
-let avgPopulation = 33;
-console.log('4: ', population < avgPopulation);
+const avgPopulation = 33;
+console.log("4: ", population < avgPopulation);
 
 let description = country;
-description += ' is in ';
+description += " is in ";
 description += continent;
-description += ', and its ';
+description += ", and its ";
 description += population;
-description += ' million people speak ';
+description += " million people speak ";
 description += language;
-console.log('5: ', description);
+
+// LECTURE: Strings and Template Literals
+description = `${country} is in ${continent}, \
+and its ${population} million people speak ${language}.`;
+console.log("5: ", description);
+
+// LECTURE: Taking Decisions: if / else Statements
+if (population > avgPopulation) {
+  console.log(`${country}'s population is above average.`);
+} else {
+  const populationDiff = avgPopulation - population;
+  console.log(
+    `${country}'s population is ${populationDiff} million \
+below average.`
+  );
+}
+
+// LECTURE: Type Conversion and Coercion
+console.log("9" - "5"); // expect 4
+console.log("19" - "13" + "17"); // expect '617'
+console.log("19" - "13" + 17); // expect 23
+console.log("123" < 57); // expect false
+console.log(5 + 6 + "4" + 9 - 4 - 2); // expect 1143
+
+// LECTURE: Equality Operators: == vs. ===
+//const numNeighbours = Number(
+//  prompt("How many neighbour countries does your country have?")
+//);
+//
+//if (numNeighbours === 1) console.log("Only 1 border");
+//else if (numNeighbours > 1) console.log("More than 1 border");
+//else console.log("No borders");
+//
+
+// LECTURE: Logical Operators
+const speakEnglish = language === "English";
+const lessPopulation = population < 50;
+
+if (speakEnglish && lessPopulation && !isIsland)
+  console.log(`You shoud live in ${country}:)`);
+else console.log(`${country} does not meet your criteria:(`);
+
+// LECTURE: The switch Statement
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("MOST number of native speakers");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D");
+}
+
+// LECTURE: The Conditional (Ternary) Operator
+const descPopulation = population > 33 ? "above" : "below";
+console.log(`${country}'s population is ${descPopulation} average.`);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -41,9 +41,8 @@ console.log(lastName + ' ' + firstName);
 let x = y = 5;
 console.log(x, y); 
 
-*/
 
-// Challenge 1
+// Coding Challenge 1
 //const markMass = 78;
 //const markHeight = 1.69;
 //
@@ -62,3 +61,191 @@ const markHigherBMI = markBMI > joneBMI;
 console.log('Challenge 1 answers: ', markBMI, joneBMI, markHigherBMI)
 
 
+
+const firstName = 'Minwoo';
+const job = 'Software Developer';
+
+const introduceMyself = `I'm ${firstName}, a ${job}.`;
+console.log(introduceMyself);
+
+console.log('Multiple \n links');
+console.log(`Multiple
+ links`);
+
+
+const age = 30;
+
+if (age >= 18) {
+  console.log("Hi");
+} else {
+  console.log("Bye");
+}
+
+
+// Coding Challenge 2
+const markMass = 95;
+const markHeight = 1.88;
+
+const joneMass = 85;
+const joneHeight = 1.76;
+
+const markBMI = markMass / markHeight ** 2;
+const joneBMI = joneMass / joneHeight ** 2;
+
+const markHigherBMI = markBMI > joneBMI;
+
+if (markHigherBMI) {
+  console.log(`Mark's BMI (${markBMI}) is higher than John's (${joneBMI})!`);
+} else {
+  console.log(`John's BMI (${joneBMI}) is higher than Mark's (${markBMI})`);
+}
+
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear));
+console.log(inputYear + 1);
+
+console.log(Number("Minwoo"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log("I am " + 23 + " years old.");
+console.log("23" - "10" - 3);
+console.log("23" * "2");
+console.log("17" > "18");
+
+let n = "1" + 1; // '11'
+n = n - 1; // 10
+console.log(n);
+
+
+// 5 falsy values
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(""));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+
+console.log(typeof undefined);
+
+// Type coercion
+const money = 0;
+if (money) {
+  console.log("Yeah!");
+} else {
+  console.log("Oh my god");
+}
+
+let height;
+
+if (height) {
+  console.log("Defined");
+} else {
+  console.log("Undefined");
+}
+
+
+const age = 18;
+if (age === 18) console.log("Hello age");
+console.log(18 == "18"); // loose equal (type coertion: string -> number)
+console.log(18 === "18"); // strict equal
+
+// prompt
+const realAge = prompt("Enter your age.");
+console.log(realAge, typeof realAge);
+
+
+// LOGICAL OPERATORS
+const hasDriversLicense = true;
+const hasGoodVision = false;
+
+console.log(hasDriversLicense && hasGoodVision); // AND
+console.log(hasDriversLicense || hasGoodVision); // OR
+console.log(!hasGoodVision); // NOT
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+if (shouldDrive) {
+  console.log("Go to drive~!");
+} else {
+  console.log("You must not drive");
+}
+
+
+// Coding Challenge 3
+//const avgScoreDolphins = (96 + 108 + 89) / 3;
+//const avgScoreKoalas = (88 + 91 + 110) / 3;
+
+const avgScoreDolphins = (97 + 112 + 101) / 3;
+const avgScoreKoalas = (109 + 95 + 50) / 3;
+
+//const avgScoreDolphins = (97 + 112 + 101) / 3;
+//const avgScoreKoalas = (109 + 95 + 106) / 3;
+const minScore = 100;
+
+if (avgScoreDolphins > avgScoreKoalas && avgScoreDolphins >= minScore)
+  console.log("Dolphins win!");
+else if (avgScoreDolphins < avgScoreKoalas && avgScoreKoalas >= minScore)
+  console.log("Koalas win!");
+else if (avgScoreDolphins === avgScoreKoalas && avgScoreKoalas >= minScore)
+  console.log("Draw!");
+else console.log("No team wins....");
+
+const day = "mon";
+
+// Switch statement
+switch (day) {
+  case "mon":
+  case "tue":
+  case "wed":
+  case "thu":
+    console.log("Sad!");
+    break;
+  case "fri":
+    console.log("Happy!");
+    break;
+  case "sat":
+  case "sun":
+    console.log("Very Happy!");
+    break;
+  default:
+    console.log("Invalid day");
+}
+
+// if - else statement
+if (day === "mon" || day === "tue" || day === "wed" || day === "thu")
+  console.log("Sad!");
+else if (day === "fri") console.log("Happy!");
+else if (day === "sat" || day === "sun") console.log("Very Happy!");
+else console.log("Invalid day");
+
+
+// Expression
+3 + 4;
+1991;
+true && false;
+
+// Statement (Does not produce values, instead performs some actions)
+if (23 > 10) {
+  const str = "23 is bigger";
+}
+
+
+// Conditional Operator
+const age = 23;
+age >= 20 ? console.log("You are too old") : console.log("You are so young!");
+const adj = age >= 20 ? "old" : "young";
+
+
+// Coding Challenge 4
+const bill = 430;
+const tipRatio = (bill >= 50 && bill <= 300 ? 15 : 20) / 100;
+const tip = bill * tipRatio;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`
+);
+
+*/
