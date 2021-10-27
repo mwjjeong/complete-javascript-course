e3b4e1e61b424e2a26b8be1d41b7933424c70980
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-10-27 17:15:09 +0900

    Progress to learn Section 6
    * Learn the definition of DOM and Events
    * Learn how to manipulate the DOM a little bit
    * Finished the lecture 73

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+/*
+// Select a document object
+const doc = document.querySelector('.message');
+console.log(doc);
+console.log(typeof doc);
+
+const text = doc.textContent;
+console.log(text);
+
+// Basic object
+const obj = {
+  a: 1,
+  b: 2,
+};
+console.log(obj);
+console.log(typeof obj);
+console.log(typeof document);
+console.log(document);
+
+// Set the text to the document object
+let newText = doc.textContent;
+doc.textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+const guess = document.querySelector('.guess');
+guess.value = 100;
+*/
+
+const guessInput = document.querySelector('.guess');
+const messageElement = document.querySelector('.message');
+const originalText = messageElement.textContent;
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(guessInput.value);
+
+  if (!guess) messageElement.textContent = 'No number!';
+  else messageElement.textContent = originalText;
+  console.log('Input:', guess);
+});
