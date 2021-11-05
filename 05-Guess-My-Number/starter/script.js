6820a82d2c7cf19fffd043f39d0d4f73f0389998
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-11-05 15:48:14 +0900

    Progress to learn section 6
    * Finished the lecture 74

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -28,16 +28,39 @@ document.querySelector('.score').textContent = 20;
 
 const guess = document.querySelector('.guess');
 guess.value = 100;
+
 */
 
 const guessInput = document.querySelector('.guess');
 const messageElement = document.querySelector('.message');
 const originalText = messageElement.textContent;
+const scoreElement = document.querySelector('.score');
+const highscoreElement = document.querySelector('.highscore');
+const numberElement = document.querySelector('.number');
 
-document.querySelector('.check').addEventListener('click', () => {
+let score = Number(scoreElement.textContent);
+let highscore = Number(highscoreElement.textContent);
+const secretNum = Math.trunc(Math.random() * 20) + 1;
+numberElement.textContent = secretNum;
+
+const gameLogic = () => {
+  if (score === 0) return;
   const guess = Number(guessInput.value);
 
-  if (!guess) messageElement.textContent = 'No number!';
-  else messageElement.textContent = originalText;
+  if (!guess) {
+    messageElement.textContent = 'No number!';
+  } else if (guess === secretNum) {
+    messageElement.textContent = 'Correct Number!';
+    if (highscore < score) highscore = score;
+  } else {
+    if (guess > secretNum) messageElement.textContent = 'Too High :(';
+    else messageElement.textContent = 'Too Low :(';
+    score--;
+  }
   console.log('Input:', guess);
-});
+  scoreElement.textContent = score;
+  highscoreElement.textContent = highscore;
+
+  if (score === 0) messageElement.textContent = 'You lost the game!';
+};
+document.querySelector('.check').addEventListener('click', gameLogic);
