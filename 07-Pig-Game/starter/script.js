e299f9799be7bfb475c85a8c8b72901b28fb0e96
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-08 21:16:35 +0900

    Implement function of the 'New Game' button

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const btnNewGame = document.querySelector('.btn--new');
+
+function resetPlayerScore(player) {
+  player.querySelector('.score').textContent = 0;
+  player.querySelector('.current-score').textContent = 0;
+}
+
+function resetGame() {
+  resetPlayerScore(document.querySelector('.player--0'));
+  resetPlayerScore(document.querySelector('.player--1'));
+}
+
+btnNewGame.addEventListener('click', resetGame);
