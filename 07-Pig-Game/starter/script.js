a532ad5500a759e26b8d6b64dd32acb98c6a4472
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-08 21:27:32 +0900

    Implement the function to role the dice

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const btnNewGame = document.querySelector('.btn--new');
-
+const btnRoleDice = document.querySelector('.btn--roll');
 function resetPlayerScore(player) {
   player.querySelector('.score').textContent = 0;
   player.querySelector('.current-score').textContent = 0;
@@ -12,4 +12,14 @@ function resetGame() {
   resetPlayerScore(document.querySelector('.player--1'));
 }
 
+function roleDice() {
+  const diceNum = getRandomNum(6);
+  document.querySelector('.dice').src = `dice-${diceNum}.png`;
+}
+
+function getRandomNum(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
+
 btnNewGame.addEventListener('click', resetGame);
+btnRoleDice.addEventListener('click', roleDice);
