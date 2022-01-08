9f37481e9b3e592b5a67dbad2a5890f7d9074014
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-08 19:55:59 +0900

    Handle an 'ESC' key press event

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -21,3 +21,7 @@ for (let btnOpenModal of btnsOpenModal) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
