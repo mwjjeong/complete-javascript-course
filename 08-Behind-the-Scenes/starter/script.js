a55fe152a8c2a63d49868e9b06b34814b8a7658f
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-28 18:30:58 +0900

    Do the practice for the scope chain

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  console.log(firstName);
+
+  function printAge() {
+    const output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1901 && birthYear <= 1996) {
+      var millenial = true;
+      // create new variable with the same name as outer scope
+      const firstName = 'Steven';
+      const str = `Oh, and you' re a millenial, ${firstName}.`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+      // reassigning
+      output = 'New output';
+    }
+    console.log(firstName);
+    console.log(millenial);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Minwoo';
+calcAge(1995);
