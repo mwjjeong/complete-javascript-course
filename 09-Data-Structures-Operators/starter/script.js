ac15a34cb9ae104b03249d9f355ab34f37f6c9fe
Minwoo Jeong <jeongmwj@gmail.com>
2022-02-23 12:37:29 +0900

    Practice the array destructuring

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -26,4 +26,36 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+let [first, , second] = restaurant.categories;
+console.log(first, second); // italian, vegetarian
+
+// swap using destructuring array
+[first, second] = [second, first];
+console.log(first, second); // vegetarian, italian
+
+let [xx, yy] = restaurant.order(2, 0);
+console.log(xx, yy);
+
+// destructuring nested array
+const nested = [1, 2, [3, 4]];
+const [aa, bb, [cc, dd]] = nested;
+console.log(aa, bb, cc, dd);
+
+// default values of array items
+const [p = 1, q = 2, r = 3] = [4, 5];
+console.log(p, q, r); // 4, 5 ,3
+*/
