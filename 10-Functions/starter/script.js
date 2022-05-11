f2ba68b54416c3e32801dde80ddca7ef5c21cfc2
Minwoo Jeong <mwjeong.sci@gmail.com>
2022-05-11 17:30:37 +0900

    Practice functions accepting callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -24,7 +24,6 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 4);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
- */
 
 const flight = 'LH234';
 const minwoo = {
@@ -57,3 +56,31 @@ const newPassport = function (person) {
 
 newPassport(minwoo);
 checkIn(flight, minwoo);
+
+ */
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('Hi!');
+};
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Minwoo', 'Jin'].forEach(high5);
