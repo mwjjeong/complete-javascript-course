15a365fddb107c551033739d0a1b691e044debce
Minwoo Jeong <mwjeong.sci@gmail.com>
2022-05-11 17:05:05 +0900

    Practice passing arguments

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const bookings = [];
+/* const bookings = [];
 
 const createBooking = function (
   flightNum,
@@ -24,3 +24,36 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 4);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+ */
+
+const flight = 'LH234';
+const minwoo = {
+  name: 'Minwoo Jeong',
+  passport: 123456789,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 123456789) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, minwoo);
+// console.log(flight);
+// console.log(minwoo); // 'name' has been changed!
+
+// // Is the same as doing...
+// const flightNum = flight; // Simply copying
+// const passenger = minwoo; // Just copying the reference to that object
+
+const newPassport = function (person) {
+  person.passport = Math.random() * 10000000000;
+};
+
+newPassport(minwoo);
+checkIn(flight, minwoo);
