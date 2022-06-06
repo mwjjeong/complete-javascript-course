2cded95ca5d5966bde0fd5f3c686e224a6059b4e
Minwoo Jeong <jeongmwj@gmail.com>
2022-06-06 22:29:42 +0900

    Complete the coding challange 2

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -382,4 +382,9 @@ GOOD LUCK 😀
 (function () {
   const header = document.querySelector('h1');
   header.style.color = 'red';
+
+  const body = document.querySelector('body');
+  body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
 })();
