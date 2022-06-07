07fb83f7d1856888e4239859ad6b1695b2e30b62
Minwoo Jeong <mwjeong.sci@gmail.com>
2022-06-07 14:59:53 +0900

    Practice the logical assignment operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -220,7 +220,6 @@ if (obj2.add) {
 /*
 const result = obj2.add && obj2.add(1, 2);
 console.log(result);
-*/
 
 // Nullish Coalescing Operator
 
@@ -236,3 +235,38 @@ console.log(d);
 // Return the first that is not nullish
 const correctD = obj.d ?? 10;
 console.log(correctD);
+
+*/
+
+// Logical Assignment Operators (ES2021)
+const rest1 = {
+  name: 'Capri',
+  numGuests: 20,
+};
+
+const rest2 = {
+  name: 'La Pizza',
+  owner: 'Giovanni Rossi',
+};
+
+// short circuiting
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// new way!
+// OR assignment operator (same behavior with OR short circuiting)
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+// AND assignment operator
+rest1.owner &&= '<ANONYMOUS>'; // nothing happened because the variable does not exist
+rest2.owner &&= '<ANONYMOUS>';
+console.log(rest1);
+console.log(rest2);
