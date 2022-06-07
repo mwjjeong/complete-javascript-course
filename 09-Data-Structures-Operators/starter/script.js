6b2479878173a36cc12162ff6ad221bc3d7705c7
Minwoo Jeong <mwjeong.sci@gmail.com>
2022-06-07 16:56:10 +0900

    Practice the way to loop arrays: for-of loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -292,6 +292,7 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 GOOD LUCK 😀
 */
 
+/*
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -368,3 +369,42 @@ console.log(printGoals(...['Davies', 'Muller', 'Lewandowski', 'Kimmich']));
 
 // 7
 console.log((team1 < team2 || 'team1') && 'team2');
+*/
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Looping Arrays: for-of loop
+for (const item of menu) console.log(item);
+
+// when we need indexes
+for (const [i, item] of menu.entries()) {
+  console.log(`${i + 1}: ${item}`);
+}
+
+console.log([...menu.entries()]); // [index, element]
