d8f72ec84b48527b4f035edc50065ebf0cdcf809
Minwoo Jeong <jeongmwj@gmail.com>
2022-06-11 15:42:36 +0900

    Solve the code challenge 4

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -761,6 +761,7 @@ for (const [min, event] of gameEvents) {
 
 // String
 
+/*
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -936,3 +937,56 @@ const planesInLine = function (n) {
   console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`);
 };
 planesInLine(10);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value.trim();
+  const variables = text.split('\n');
+
+  for (const [i, variable] of variables.entries()) {
+    const words = variable.trim().toLowerCase().split('_');
+    const camelCase = [];
+
+    for (const [j, w] of words.entries()) {
+      j == 0
+        ? camelCase.push(w)
+        : camelCase.push(w.replace(w[0], w[0].toUpperCase()));
+    }
+    console.log(`${camelCase.join('').padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
